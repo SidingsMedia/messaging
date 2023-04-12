39c9aa6811af0ea3d1ab8dd94337e62c53d954f8
Matthew Nickson <mnickson@sidingsmedia.com>
2023-04-12 17:39:29 +0100

    Added option for trusted proxies
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- util/config.go
+++ util/config.go
@@ -11,18 +11,28 @@ import (
 
 // Runtime config
 var (
+  // Address to bind server to
 	BindAddr string
+  // Proxies from which to trust alternative client IP headers
+  TrustedProxies []string
+  // Email to send messages from
   EmailFrom string
+  // Email to send messages to
 	EmailTo string
+  // Address or hostname of SMTP server
   SMTPAddr string
+  // Port to submit mail on
   SMTPPort int
+  // User to log into SMTP server with
   SMTPUsr string
+  // Password to use to authenticate with SMTP server
   SMTPPwd string
 )
 
 // Environment variables
 const (
 	BindAddrEnv = "BIND_ADDRESS"
+  TrustedProxiesEnv = "TRUSTED_PROXIES"
   EmailFromEnv = "EMAIL_FROM"
   EmailToEnv = "EMAIL_TO"
   SMTPAddrEnv = "SMTP_ADDRESS"
@@ -34,6 +44,7 @@ const (
 // Defaults
 const (
 	DefaultBindAddr = "[::]:3000"
+  DefaultTrustedProxies = "*"
   DefaultSMTPPort = 587
 )
 
